9ab0ba8d3cab44740e30b4c1dd3f590707439c15
SwapnilKapoor1 <swapnilkapoor341@gmail.com>
2024-02-11 00:23:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ function displaySearchResults(meals) {
 // adding the meal to the list
 
 function addToFavourites(meal) {
+    savedFavouriteMeals = savedFavouriteMeals || [];
     savedFavouriteMeals.push(meal.strMeal);
     localStorage.setItem('favouriteMeals', JSON.stringify(savedFavouriteMeals));
     message.setAttribute("style","visibility:visible"); //displaying the message for succesful addition
